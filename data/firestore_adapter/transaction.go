38ef2af6a5cf2937d4c88141b5c6e72f50268423
Ryan <mhogar98@gmail.com>
2021-09-26 01:37:30 -0400

    #61 - Fix batch has no writes error

--- data/firestore_adapter/transaction.go
+++ data/firestore_adapter/transaction.go
@@ -8,29 +8,39 @@ import (
 type FirestoreTransaction struct {
 	FirestoreCRUD
 	Batch *firestore.WriteBatch
+
+	hasWrites bool
 }
 
 func (tx *FirestoreTransaction) Create(ref *firestore.DocumentRef, data interface{}) error {
+	tx.hasWrites = true
 	tx.Batch.Create(ref, data)
 	return nil
 }
 
 func (tx *FirestoreTransaction) Set(ref *firestore.DocumentRef, data interface{}) error {
+	tx.hasWrites = true
 	tx.Batch.Set(ref, data)
 	return nil
 }
 
 func (tx *FirestoreTransaction) Update(ref *firestore.DocumentRef, updates []firestore.Update) error {
+	tx.hasWrites = true
 	tx.Batch.Update(ref, updates)
 	return nil
 }
 
 func (tx *FirestoreTransaction) Delete(ref *firestore.DocumentRef) error {
+	tx.hasWrites = true
 	tx.Batch.Delete(ref)
 	return nil
 }
 
 func (tx *FirestoreTransaction) Commit() error {
+	if !tx.hasWrites {
+		return nil
+	}
+
 	ctx, cancel := tx.ContextFactory.CreateStandardTimeoutContext()
 	_, err := tx.Batch.Commit(ctx)
 	cancel()
